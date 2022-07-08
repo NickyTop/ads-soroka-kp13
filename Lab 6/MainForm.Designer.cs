53aa4b2c580dd9f56b011ee81f4ab1ef729ba62b
Nikita <90898233+NickyTop@users.noreply.github.com>
2022-07-08 21:12:06 +0300

    Lab 6 - done

---
+++ Lab 6/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Hanoi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTb = new System.Windows.Forms.TextBox();
+            this.resultTb = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTb
+            // 
+            this.inputTb.Font = new System.Drawing.Font("Bahnschrift Condensed", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputTb.Location = new System.Drawing.Point(149, 84);
+            this.inputTb.Name = "inputTb";
+            this.inputTb.Size = new System.Drawing.Size(282, 40);
+            this.inputTb.TabIndex = 0;
+            // 
+            // resultTb
+            // 
+            this.resultTb.Font = new System.Drawing.Font("Bahnschrift Condensed", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resultTb.Location = new System.Drawing.Point(35, 221);
+            this.resultTb.Name = "resultTb";
+            this.resultTb.Size = new System.Drawing.Size(475, 195);
+            this.resultTb.TabIndex = 1;
+            this.resultTb.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(216, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Initial data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(244, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiLight Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(564, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 89);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GreenYellow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift SemiLight Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(564, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 89);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "PreSetup";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultTb);
+            this.Controls.Add(this.inputTb);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTb;
+        private System.Windows.Forms.RichTextBox resultTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab 6/MainForm.cs
@@ -0,0 +1,86 @@
+﻿using Hanoi.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hanoi
+{
+    public partial class MainForm : Form
+    {
+        private Stack A { get; set; }
+        private Stack B { get; set; }
+        private Stack C { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async Task Move(int discs, Stack from, Stack to, Stack interchange)
+        {
+            if (discs > 0)
+            {
+                await Move(discs - 1, from, interchange, to);
+                await MoveDisc(from, to);
+                await Move(discs - 1, interchange, to, from);
+            }
+        }
+
+        private async Task MoveDisc(Stack from, Stack to)
+        {
+            var temp = from.Pop();
+            to.Push(temp);
+
+            await RefreshTextbox();
+        }
+
+        private async Task RefreshTextbox()
+        {
+            this.resultTb.Text = $"A = {this.A.ToString()}\nB = {this.B.ToString()}\nC = {this.C.ToString()}";
+
+            await Task.Delay(500);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.A = new Stack();
+                this.B = new Stack();
+                this.C = new Stack();
+
+                var data = this.inputTb.Text.Split(" ").Select(x => Convert.ToInt32(x)).ToList();
+
+                // validation
+                for (int i = 1; i < data.Count; i++)
+                {
+                    if (data[i - 1] < data[i])
+                    {
+                        throw new ArgumentException("Elements should be in the decreasing order!");
+                    }
+                }
+                foreach (var value in data)
+                {
+                    this.A.Push(value);
+                }
+
+                await Move(data.Count, A, C, B);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error! {ex.Message}.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.inputTb.Text = "9 6 4 2 1";
+        }
+    }
+}
