94cb062ee69d18fafdbdade8ff2bbd5ed9db87e1
Nikita <90898233+NickyTop@users.noreply.github.com>
2022-07-08 21:09:16 +0300

    Lab 5 - done

---
+++ Лаб5 АСД/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Лаб5_АСД
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
